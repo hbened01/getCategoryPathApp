226d09b6915ce24d66a61d7f85e2340029e504ce
Humberto Benedito <humbertobenedito@gmail.com>
2025-06-23 15:52:30 +0200

    feat/add base url deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { resolve } from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://github.com/hbened01/getCategoryPathApp',
   plugins: [vue()],
   resolve: {
     alias: {
