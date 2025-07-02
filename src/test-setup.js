5f9220b08b4c36c2c90231f80af9d03f050d8e94
Humberto Benedito <humbertobenedito@gmail.com>
2025-07-02 15:29:13 +0200

    feat/delete duplicates files

--- src/test-setup.js
+++
@@ -1,9 +0,0 @@
-import { config } from '@vue/test-utils';
-// Global test configuration
-config.global.stubs = {
-// Add any global component stubs here if needed
-};
-// Mock any global properties if needed
-config.global.mocks = {
-// Add global mocks here if needed
-};
